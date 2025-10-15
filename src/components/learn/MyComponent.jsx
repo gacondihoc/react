cb6ad0abe1c6994811d0e15f046a217e26292c49
Sang's GIT <sangjp.22@gmail.com>
2025-10-15 16:46:39 +0900

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,21 @@
 
 import './style.css';
 const MyComponent = () => {
+    const hoidanit = "eric jsx data"; //string
+    const age = 25;
+    const boo = true;
+    const unde = undefined;
+    const nu = null;
+    const arr = [1, 2, 3]
+    const ob = {
+        name: "hoidanit",
+        age: 25
+    }
     return (
         <>
-            <div>eric & hoidanit</div>
+            <div>{hoidanit} & hoidanit & {age} age & {boo}& {unde}& {nu} &{JSON.stringify(arr)}&{JSON.stringify(ob)}</div>
+            <div>{console.log("ERIC")}</div>
+
             <div className="child"
                 style={
                     {

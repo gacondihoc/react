02c52e8eb4966b8e51014fa428d3120cdd819640
Sang's GIT <sangjp.22@gmail.com>
2025-10-16 13:39:17 +0900

    video34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,16 @@
+// この書き方は初心者にはまだ少しい難しいと思って、学び始めたばかりなので、まずはpropsが何なのかをしっかり理解する必要があります。
+// const TodoData = ({ name,age,data }) => {
+const TodoData = (props) => {
+    //props is a object
+    // object destructing
+    const { name, age, data } = props;
+    // console.log(">>>> chekc props: ", props)
+    return (
+        <div className='todo-data'>
+            <div>My name is {name}</div>
+            <div>Learning React</div>
+            <div>Watching Youtube</div>
+        </div>
+    )
+}
+export default TodoData;

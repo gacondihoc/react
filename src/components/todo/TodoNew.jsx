fc1480533b2c4c35e5f46382cd993e9583182b55
Sang's GIT <sangjp.22@gmail.com>
2025-10-16 15:30:24 +0900

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,10 +2,23 @@ const TodoNew = (props) => {
     console.log(props)
     const { addNewTodo } = props
     // addNewTodo("eric")
+
+    const handleClick = () => {
+        alert("click me")
+    }
+    const handleOnChange = (name) => {
+        console.log(">>>> handle onChange")
+        console.log(name)
+    }
+
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }

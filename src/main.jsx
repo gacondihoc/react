6c283bf09b24d785ab24ae6bc98331f7f4d94409
Sang's GIT <sangjp.22@gmail.com>
2025-10-20 14:35:12 +0900

    video 45 tich hop router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>
+  }
+  ,
+  {
+    path: "/register",
+    element: <div>Register page</div>
+  }
+  ,
+  {
+    path: "/users",
+    element: <div>Users page</div>
+  },
+  {
+    path: "/products",
+    element: <div>Products page</div>
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
